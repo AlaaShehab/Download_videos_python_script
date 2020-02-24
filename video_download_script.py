bd942f857ce39b2e0c0e72d9f0486d95439f7d28
Alaa Shehab <alaa.shehab.95@gmail.com>
2020-02-24 21:16:18 +0200

    Update video_download_script.py

--- video_download_script.py
+++ video_download_script.py
@@ -4,6 +4,6 @@ def download():
     urls = open('vid_list', 'r')
     urls.read()
     urls.close()
-    os.system("youtube-dl -o Videos/%(title)s-%(id)s.%(ext)s' -a vid_list --recode-video mp4")
+    os.system("youtube-dl -o Videos/%(title)s-%(id)s -a vid_list --recode-video mp4")
 
 download()
